f4503b3d4b3c69b988f617e428935190f230482b
Ринат Мухитов <malayka_73@inbox.ru>
2023-11-27 00:14:44 +0400

    Bootstrap

--- classviewshome/models.py
+++ classviewshome/models.py
@@ -4,13 +4,13 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     full_name = models.CharField(max_length=250, verbose_name="ФИО", blank=True, null=True)
     birth_date = models.DateField(verbose_name="Дата рождения", blank=True, null=True)
-    image_profile = models.ImageField(upload_to='profile/image/', verbose_name='Фото профиля')
+    image_profile = models.ImageField(upload_to='image/', verbose_name='Фото профиля')
     about_me = models.TextField(verbose_name='О себе', blank=True)
 
 class Team(models.Model):
     first_name = models.CharField(max_length=250, verbose_name="Имя", blank=True, null=True)
     second_name = models.CharField(max_length=250, verbose_name="Фамилия", blank=True, null=True)
-    image_profile1 = models.ImageField(upload_to='image', verbose_name='Фото профиля')
+    image_profile1 = models.ImageField(upload_to='image/', verbose_name='Фото профиля')
     your_country = models.CharField(max_length=250, verbose_name="Страна", blank=True, null=True)
     your_city = models.CharField(max_length=250, verbose_name="Город", blank=True, null=True)
 
